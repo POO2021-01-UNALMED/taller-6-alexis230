4ccbcbc795888abaae3fe315df7ddf8b761b1201
alexis <alexgarca31@gmail.com>
2021-04-20 23:22:41 -0500

    casi
    a

---
+++ src/vehiculos/Automovil.java
@@ -0,0 +1,12 @@
+package vehiculos;
+
+public class Automovil extends Vehiculo {
+    int puestos;
+    static int fabricados;
+    
+    public Automovil(String placa, String nombre, int precio, int peso,Fabricante fabricante,int puestos) {
+       super(placa,4,100,nombre,precio,peso,"FWD",fabricante);
+       this.puestos=puestos;
+       fabricados++;
+    }
+}
